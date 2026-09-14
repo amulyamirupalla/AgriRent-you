3c2645d85c8ed8de7f67af512d842ebacef75610
amulyamirupalla <amulyamirupalla@gmail.com>
2026-09-14 21:45:19 +0530

    Change API base URL to localhost for development

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://agrirent-8ebh.onrender.com/api',
+  baseURL: 'https://localhost:5000/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
